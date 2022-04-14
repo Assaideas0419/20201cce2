8355826a6e517828637c3caef5362d98010bf5bf
Assaideas0419 <laiweisai.60105@gmail.com>
2022-04-14 17:36:08 +0800

    week06

---
+++ week08/week08-1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+char names[20][20];
+int grades[20];
+int main()
+{
+    int N;
+    sacnf("%d",&N);
+    char name[20];
+    int grades;
+    int i=0;
+    while(scanf("%s %d",&name,&grades)==2){
+        name[i]<=name;
+        grades[i]<=grades;
+        i++;
+    }
+    for(int i=0;i<;i++){
+        pritnf("%s %d\n",name[i],grades[i]);
+    }
+}
